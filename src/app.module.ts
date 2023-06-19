587802da94487b20904699ac4e64a22dbe2eb641
Pashio84 <yuki.handa@sun-asterisk.com>
2023-06-19 17:28:55 +0900

    Setup Authentication service template and install JWT

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
-import { Module } from '@nestjs/common';
+// Application common modules
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Module } from '@nestjs/common';
+
+// Unique modules
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
