a6ff97c89020e7afe6fffd7d7a28f7810a416dcf
Pashio84 <yuki.handa@sun-asterisk.com>
2023-06-22 15:00:15 +0900

    Setup i18n for NestJS

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { Module } from '@nestjs/common';
 // Unique modules
 import { AuthModule } from './auth/auth.module';
 import { MailModule } from './mail/mail.module';
+import { AppI18nModule } from './i18n/i18n.module';
 
 @Module({
-  imports: [AuthModule, MailModule],
+  imports: [AuthModule, MailModule, AppI18nModule],
   controllers: [AppController],
   providers: [AppService],
 })
