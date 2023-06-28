7fe446c31f957541db5000dc5e314e1a7d5747e4
Pashio84 <yuki.handa@sun-asterisk.com>
2023-06-28 14:55:13 +0900

    Add Prisma client and setup models

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,17 @@
-// Application common modules
+// Import application common modules
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Module } from '@nestjs/common';
 
-// Unique modules
+// Import unique modules
 import { AuthModule } from './auth/auth.module';
 import { MailModule } from './mail/mail.module';
 import { AppI18nModule } from './i18n/i18n.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [AuthModule, MailModule, AppI18nModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
